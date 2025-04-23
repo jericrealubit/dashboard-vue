f2993cad87ef984b8af9166176a2fe815dc6696f
jeric realubit <jericrealubit@gmail.com>
2025-04-24 11:27:23 +1200

    fix tailwindcss v4 installation

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  server: {
-    port: 3000,
+  plugins: [vue(), vueDevTools(), tailwindcss()],
+  css: {
+    postcss: './postcss.config.js',
   },
+
+  server: { port: 3000 },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
